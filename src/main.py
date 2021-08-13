517be1165465ed2413557fea66d753cfd698f6e0
amandagnecco <amandagnecco@gmail.com>
2021-08-13 09:16:19 -0300

    adding new routes

--- src/main.py
+++ src/main.py
@@ -2,10 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+
 @app.get('/')
 def index():
-    return {'data' : {'name': 'Name_Here'}}
+    return {'data' : 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data' : 'about page'}
+
+
+@app.get('/blog/umpuplished')
+def unpublished():
+    return {'data': 'all umpublished blogs'}
+
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data' : id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data' : {'1', '2'}}
