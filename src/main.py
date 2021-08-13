66178730b6f1016bc6128ae23a18344402849f06
amandagnecco <amandagnecco@gmail.com>
2021-08-13 09:40:01 -0300

    adding parametesrs to /blog

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
 
-@app.get('/')
-def index():
-    return {'data' : 'blog list'}
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    # returns only 10 published blogs by default
+    if published:
+        return {'data' : f'{limit} published blogs from db'}
+    else:
+        return {'data' : f'{limit} blogs from db'}
 
 
 
-@app.get('/blog/umpuplished')
+@app.get('/blog/umpublished')
 def unpublished():
     return {'data': 'all umpublished blogs'}
 
